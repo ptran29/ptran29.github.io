3280f628b0ebe144b4a38e2e5d45385b997e44dc
ptran29 <35343402+ptran29@users.noreply.github.com>
2018-02-08 11:52:47 -0600

    updated

---
+++ js/code-assignment-03.js
@@ -0,0 +1,9 @@
+let projectId = 1
+let projectTitle = "Erathquake Map"
+let thumbnail = true
+let projectTitles = ["Earthquakes Map" , "Map of Earthquakes of magnitude 2.5 or greater"]
+
+console.log(projectId)
+console.log(projectTitle)
+console.log(thumbnail)
+console.log(projectTitles)
