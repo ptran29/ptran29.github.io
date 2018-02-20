06af574ec9f3b58db4eabd62886be26e35788c00
ptran29 <35343402+ptran29@users.noreply.github.com>
2018-02-20 11:42:35 -0600

    Updated

---
+++ js/code-assignment-04.js
@@ -0,0 +1,42 @@
+let projectArray = [
+  1,
+  'https://example.com',
+  'Project Title',
+  'Project description.',
+  true
+]
+
+let projectObject = {
+    id: 1,
+    url: 'https://example.com',
+    title: 'Project Title',
+    desc: 'Project description.',
+    thumb: true,
+  }
+
+let projectArray = [
+    1,
+    'https://example.com',
+    'Project Title',
+    'Project description.',
+    true,
+    ['keyword here', 'buzzword', 'another keyword']
+  ]
+
+let projectObject = {
+    id: 1,
+    url: 'https://example.com',
+    title: 'Project Title',
+    desc: 'Project description.',
+    thumb: true,
+    keywords: [
+      'keyword here',
+      'buzzword',
+      'another keyword'
+    ]
+  }
+
+
+console.log(projectArray[2])
+console.log(projectObject.title)
+console.log(projectObject["title"])
