fb4901eb70bff658d5897d9f7d18676081801a9e
ptran29 <35343402+ptran29@users.noreply.github.com>
2018-02-27 10:53:49 -0600

    updated

--- js/code-assignment-05.js
+++ js/code-assignment-05.js
@@ -1,21 +1,24 @@
-let projects = [ {
+let projectData = []
+
+projectData[0] = {
   id: 1,
   url: 'http://arcg.is/2rBHUeZ',
   title: 'Book Assignment 01',
   desc: 'Tour of Ho Chi Minh City, which was where I was born.',
   thumb: true,
   keywords: ['Ho Chi Minh City', 'story map', 'tour']
- } ,
- {
+ }
+
+ projectData[1] = {
  id: 2,
  url: 'https://www.arcgis.com/apps/View/index.html?appid=a8c111473e1c439e8ad83db8d026f587',
  title: 'Book Assignment 02',
  desc: 'Web App that shows 2.5+ Magnitude Earthquakes around the world in the past 24 hours',
  thumb: false,
  keywords: ['Earthquakes', 'USGS', 'ArcGIS']
-} ]
+}
 
-for (let i=0; i < projects.length; i++) {
+for (let i = 0; i < projectData.length; i++) {
   let x = i + 1
   console.log('BookAssignment0-' + x + '.png');
 }
