9498fcde431c40d7ad5b63739028c9a226b0ecb0
ptran29 <35343402+ptran29@users.noreply.github.com>
2018-03-20 11:44:50 -0500

    updated

--- js/code-assignment-06a.js
+++ js/code-assignment-06a.js
@@ -9,8 +9,27 @@ projectData[0] = {
   keywords: ['Ho Chi Minh City', 'story map', 'tour']
  }
 
+ projectData[1] = {
+ id: 2,
+ url: 'https://www.arcgis.com/apps/View/index.html?appid=a8c111473e1c439e8ad83db8d026f587',
+ title: 'Book Assignment 02',
+ desc: 'Web App that shows 2.5+ Magnitude Earthquakes around the world in the past 24 hours',
+ thumb: false,
+ keywords: ['Earthquakes', 'USGS', 'ArcGIS']
+}
 
 for (let i = 0; i < projectData.length; i++) {
   createTitle(projectData[i].title)
   createThumbnail(projectData[i].thumb, projectData[i].id)
+}
+
+function createTitle (title) {
+  console.log('Title:' + title)
+}
+function createThumbnail (thumb, id) {
+  if (thumb === true) {
+    console.log('BookAssignment0-' + id + '.png')
+  } else {
+    console.log('logo.png')
+  }
 }
---
+++ js/code-assignment-07a.js
@@ -0,0 +1,24 @@
+let projectData = []
+
+projectData[0] = {
+  id: 1,
+  url: 'http://arcg.is/2rBHUeZ',
+  title: 'Book Assignment 01',
+  desc: 'Tour of Ho Chi Minh City, which was where I was born.',
+  thumb: true,
+  keywords: ['Ho Chi Minh City', 'story map', 'tour']
+ }
+
+ projectData[1] = {
+ id: 2,
+ url: 'https://www.arcgis.com/apps/View/index.html?appid=a8c111473e1c439e8ad83db8d026f587',
+ title: 'Book Assignment 02',
+ desc: 'Web App that shows 2.5+ Magnitude Earthquakes around the world in the past 24 hours',
+ thumb: false,
+ keywords: ['Earthquakes', 'USGS', 'ArcGIS']
+}
+
+for (let i = 0; i < projectData.length; i++) {
+  createTitle(projectData[i].title)
+  createThumbnail(projectData[i].thumb, projectData[i].id)
+}
