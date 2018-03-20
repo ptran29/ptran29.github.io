0fb6d436dfa8a9cc548f7120e95b1b2815a6a95c
ptran29 <35343402+ptran29@users.noreply.github.com>
2018-03-20 11:28:05 -0500

    updated

--- js/code-assignment-06a.js
+++ js/code-assignment-06a.js
@@ -1,15 +1,16 @@
+let projectData = []
+
+projectData[0] = {
+  id: 1,
+  url: 'http://arcg.is/2rBHUeZ',
+  title: 'Book Assignment 01',
+  desc: 'Tour of Ho Chi Minh City, which was where I was born.',
+  thumb: true,
+  keywords: ['Ho Chi Minh City', 'story map', 'tour']
+ }
+
+
 for (let i = 0; i < projectData.length; i++) {
   createTitle(projectData[i].title)
-  if (projectData[i].thumb === true) {
-    console.log('BookAssignment0-' + projectData[i].id + '.png')
-  } else {
-    console.log('logo.png')
-  }
+  createThumbnail(projectData[i].thumb, projectData[i].id)
 }
-
-
-
-function createTitle (title) {
-  console.log('Title' + title)
-}
-function createThumbnail () {}
---
+++ js/function.js
@@ -0,0 +1,10 @@
+function createTitle (title) {
+  console.log('Title:' + title)
+}
+function createThumbnail (thumb, id) {
+  if (thumb === true) {
+    console.log('BookAssignment0-' + id + '.png')
+  } else {
+    console.log('logo.png')
+  }
+}
