b76c52c036bb9f32b0a5ab0fd637b874031e11be
ptran29 <35343402+ptran29@users.noreply.github.com>
2018-04-17 11:29:46 -0500

    updated

--- BatonRougeTouristVenue/TouristVenueMap.js
+++ BatonRougeTouristVenue/TouristVenueMap.js
@@ -4,6 +4,16 @@ let PhatColorMap1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.
 let PhatLightMap1 = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(PhatMapFinal)
 let PhatDarkMap1 = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png').addTo(PhatMapFinal)
 
+let MyMarkerIcon = L.icon({
+  iconUrl: 'Venue.jpg',
+  iconSize: [320, 170], // size of the icon
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+})
+
+let PhatMarker = L.marker([30.430840, -91.378721], {icon: MyMarkerIcon}).addTo(PhatMapFinal).bindPopup('This webmap shows all the tourist venues in Baton Rouge and its owner.')
+
+
 let myBasemaps = {
   'Light Basemap': PhatLightMap1,
   'Dark Basemap': PhatDarkMap1,
@@ -23,7 +33,9 @@ PhatGeojsonStyle = function (feature) {
 function createPopup (feature, featurelayer) {
   let SiteName = feature.properties.NAME
   let Owner = feature.properties.OWNER
-  featurelayer.bindPopup('This Baton Rouge Tourist Venue is ' + SiteName + ' and it is own by ' + Owner )
+  let Lat = feature.properties.Y
+  let Long = feature.properties.X
+  featurelayer.bindPopup('This Baton Rouge Tourist Venue is ' + SiteName + ' and it is own by ' + Owner + '.')
 }
 
 PhatGeojsonOptions = {
