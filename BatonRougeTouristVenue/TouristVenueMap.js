bfb7354548d47900da6874339c7a0fd77bea908c
ptran29 <35343402+ptran29@users.noreply.github.com>
2018-04-17 11:32:26 -0500

    updated

--- BatonRougeTouristVenue/TouristVenueMap.js
+++ BatonRougeTouristVenue/TouristVenueMap.js
@@ -5,7 +5,7 @@ let PhatLightMap1 = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/
 let PhatDarkMap1 = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png').addTo(PhatMapFinal)
 
 let MyMarkerIcon = L.icon({
-  iconUrl: 'Venue.jpg',
+  iconUrl: 'Venue.png',
   iconSize: [320, 170], // size of the icon
   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
